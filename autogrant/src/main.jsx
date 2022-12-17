2bb4e1c701689f362fbb9fab1a20bc352664b0b2
Anchit Bajaj <ab@abifog.com>
2022-12-18 04:27:19 +0530

    Example to integrate with django

--- autogrant/src/main.jsx
+++ autogrant/src/main.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import MeritCombinedForm from "./components/MeritCombinedForm";
+import Merit3Form from "./components/Merit3Form";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// import "bootstrap/dist/css/bootstrap.min.css"; // remove this if using template bootstrap
+import "./assets/index.css";
+
+ReactDOM.createRoot(document.getElementById("root-m12")).render(
   <React.StrictMode>
-    <App />
+    <>
+      <h1 className="mt-5">Autogrant Merit-I & Merit-II</h1>
+      <MeritCombinedForm />
+    </>
+  </React.StrictMode>
+);
+
+ReactDOM.createRoot(document.getElementById("root-m3")).render(
+  <React.StrictMode>
+    <>
+      <h1 className="mt-5">Autogrant Merit-III</h1>
+      <Merit3Form />
+    </>
   </React.StrictMode>
 );
